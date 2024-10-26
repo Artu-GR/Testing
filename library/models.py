d95547c7d09e1d2c886fa1ac79e243637e831c37
An-Isa93 <isabel.693@outlook.es>
2024-10-26 15:51:12 -0600

    Model Book

---
+++ library/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class book(models.Model):
+    title = models.CharField(max_length=250)
+    pub_date = models.DateTimeField("date published")
+    autor = models.CharField(max_length=150)
+    genre = models.CharField(max_length=100)
+    description = models.TextField()
+    availability = models.PositiveIntegerField(default=0)
+    editorial = models.CharField(max_length=150)
+    isbn = models.CharField(max_length=13, unique=True)
+    
