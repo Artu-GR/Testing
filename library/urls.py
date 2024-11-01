2784b9ee60c8a958e7b8f4dcb59adcfecea77bb8
RebecaJara <rebeca.jaramilloc@uanl.edu.mx>
2024-11-01 16:25:07 -0600

    Lista de libros registrados

--- library/urls.py
+++ library/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns=[
     path("",views.index, name="index"),
     path('book/', views.book, name='book'),
+    path('book_list/', views.book_list, name='book_list'),
     path('client/', views.client, name='client'),
     path("<int:book_id>/book/", views.book, name="vote"),
 ]
--- library/views.py
+++ library/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+from .models import Book
 from .forms import BookForm, ClientForm
 
 def index(request):
@@ -12,10 +13,16 @@ def book(request):
         form = BookForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('') #regresa a la pagina princial
+            return redirect('book_list') #regresa a la pagina princial
     else:
         form = BookForm()
     return render(request, 'library/book.html', {'form': form})
+
+#Lista de libros registrados
+def book_list(request):
+    books = Book.objects.all()
+    print(f"Número de libros encontrados: {books.count()}")
+    return render(request, 'library/book_list.html', {'books': books})
 
 # Agregar un cliente/usuario
 def client(request):
