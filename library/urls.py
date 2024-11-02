56e4719f90a878e6300acd9fa3d68e373388ece8
RebecaJara <rebeca.jaramilloc@uanl.edu.mx>
2024-11-01 19:06:29 -0600

    Editar libro/cliente

--- library/urls.py
+++ library/urls.py
@@ -5,7 +5,9 @@ urlpatterns=[
     path("",views.index, name="index"),
     path('book/', views.book, name='book'),
     path('book_list/', views.book_list, name='book_list'),
+    path('book_edit/<int:pk>/', views.book_edit, name='book_edit'),
     path('client/', views.client, name='client'),
     path('client_list/', views.client_list, name='client_list'),
+    path('client_edit/<int:pk>/', views.client_edit, name='client_edit'),
     path("<int:book_id>/book/", views.book, name="vote"),
 ]
--- library/views.py
+++ library/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Book, Client
@@ -23,6 +23,19 @@ def book_list(request):
     books = Book.objects.all()
     return render(request, 'library/book_list.html', {'books': books})
 
+#Editar info de libros registrados
+def book_edit(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'library/book.html', {'form': form, 'book': book})
+
+
 # Agregar un cliente/usuario
 def client(request):
     if request.method == 'POST':
@@ -38,3 +51,17 @@ def client(request):
 def client_list(request):
     clients = Client.objects.all()
     return render(request, 'library/client_list.html', {'clients': clients})
+
+#Editar info de clientes registrados
+def client_edit(request, pk):
+    client = get_object_or_404(Client, pk=pk)
+    if request.method == 'POST':
+        form = ClientForm(request.POST, instance=client)
+        if form.is_valid():
+            form.save()
+            return redirect('client_list')
+        else:
+            print(form.errors)
+    else:
+        form = ClientForm(instance=client)
+    return render(request, 'library/client.html', {'form': form, 'client': client})
